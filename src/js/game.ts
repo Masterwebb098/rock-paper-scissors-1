427e7605b95e9b7a9c3a28611fca08c44b936a85
re-nanashi <jfabro3124@gmail.com>
2021-06-06 13:51:18 +0800

    update#1

---
+++ src/js/game.ts
@@ -0,0 +1,16 @@
+class Game {
+    private _playerScore: number
+    private _computerScore: number
+
+    get scores() {
+        return [this._playerScore, this._computerScore]
+    }
+
+    addScore() {
+
+    }
+
+    constructor() {
+        this._playerScore =
+    }
+}
